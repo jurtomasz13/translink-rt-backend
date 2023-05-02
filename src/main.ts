fd4ccc712065fc5609bbabcf71caac29311c8aaf
Tomasz Jurek <jur.tomasz13@gmail.com>
2023-05-02 19:10:48 +0200

    feat: enable logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
 
   app.setGlobalPrefix('api');
   app.enableVersioning();

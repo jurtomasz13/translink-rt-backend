ff8f8ada15b486954976b4cdf24863917018a744
Tomasz Jurek <jur.tomasz13@gmail.com>
2023-05-02 16:33:26 +0200

    feat: add swagger & eslint rule, enable versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning();
+
+  const config = new DocumentBuilder()
+    .setTitle('TransLink Master Recruitement Task API Specification')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
